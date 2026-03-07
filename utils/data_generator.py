89d558ea056bbf016b9ca6757cff54dc2fa41798
saafihub <ishafee07@gmail.com>
2026-03-07 17:40:32 +0800

    Add files via upload

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -1,30 +1,36 @@
 import random
 import string
 import time
+import os
+from utils.log import log
 from datetime import datetime
+from configparser import ConfigParser
 
 
 def get_random_string(length: int) -> str:
-    """
-    Generate random string with chosen length
-    :return: random string
-    """
     return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
 def get_unique_integer() -> int:
-    """
-    Generate unique integer according to current timestamp
-    :return: unique int
-    """
     return int(time.time())
 
 
 def get_current_date_and_time():
-    """
-    Generate current date in ("%d/%m/%Y %H:%M:%S") format
-    :return: current date and time
-    """
     now = datetime.now()
     formatted_date = now.strftime("%Y-%m-%d-%H:%M:%S")
     return formatted_date
+
+
+def getConfig(ckey, cvalue, BASE_DIR=None):
+    config = ConfigParser()
+    config_path = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "config.ini"
+    )
+    config.read(config_path)
+    log.info(f"{ckey}-{ckey}-{BASE_DIR}")
+    if BASE_DIR:
+        BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+        test_case_data = config.get(ckey, cvalue)
+        return os.path.join(BASE_DIR, test_case_data)
+    return config.get(ckey, cvalue)
